ea422fe4ef82ed2d1b80322612d706c3431e6729
Nikita Galaiko <nikita@galaiko.rocks>
2019-05-03 21:31:29 +0200

    more benchmarks

--- main_test.go
+++ main_test.go
@@ -5,22 +5,32 @@ import (
 	"testing"
 )
 
-func Benchmark_read_100_lines(b *testing.B) {
-	file, err := os.Open("./assets/100lines.txt")
-	if err != nil {
-		b.Fatalf("failed to read `%s`: %s", *filePath, err)
+func Benchmark_read(b *testing.B) {
+	files := []string{
+		//"./assets/10lines.txt",
+		//"./assets/100lines.txt",
+		"./assets/1000lines.txt",
 	}
 
-	wordsChan := make(chan string, 100000)
+	for _, filePath := range files {
+		b.Run(filePath, func(b *testing.B) {
+			file, err := os.Open(filePath)
+			if err != nil {
+				b.Fatalf("failed to read `%s`: %s", filePath, err)
+			}
 
-	go func() {
-		for range wordsChan {
-		}
-	}()
+			wordsChan := make(chan string, 100000)
 
-	b.ResetTimer()
+			go func() {
+				for range wordsChan {
+				}
+			}()
 
-	for i := 0; i < b.N; i++ {
-		countWords(file, wordsChan)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				countWords(file, wordsChan)
+			}
+		})
 	}
 }
