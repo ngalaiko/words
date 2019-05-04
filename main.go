90cec475d1c9fc2d3295b075700de765b4a02bca
Nikita Galaiko <nikita@galaiko.rocks>
2019-05-04 09:09:51 +0200

    v2

--- main.go
+++ main.go
@@ -6,42 +6,81 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/dgryski/go-topk"
 )
 
 var filePath = flag.String("file", "", "path to input file")
+var topN = flag.Int("n", 10, "top N words")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 	flag.Parse()
 
-	file, err := os.Open(*filePath)
-	if err != nil {
-		log.Fatalf("failed to read `%s`: %s", *filePath, err)
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
 	}
 
-	wordsChan := make(chan string)
-	doneChan := make(chan bool)
+	tk := topk.New(*topN)
+	err := fromFile(*filePath, tk)
+	for _, key := range tk.Keys() {
+		fmt.Printf("%d: %s\n", key.Count, key.Key)
+	}
 
-	go processWords(wordsChan, doneChan, 10)
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 
-	if err := countWords(file, wordsChan); err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	close(wordsChan)
-
-	<-doneChan
 }
 
-const buffSize = 2048
+func fromFile(filepath string, tk *topk.Stream) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to read `%s`: %s", filepath, err)
+	}
 
-func countWords(file *os.File, wordsChan chan<- string) error {
-	buff := make([]byte, buffSize)
+	if err := countWords(file, tk); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const batchSize = 2<<15 - 1
+
+func countWords(file *os.File, tk *topk.Stream) error {
+	stat, err := file.Stat()
+	if err != nil {
+		return err
+	}
 
 	var offset int64
 	var done bool
-	for {
+	for offset = 0; offset < stat.Size(); offset += batchSize {
+		buff := make([]byte, batchSize)
+
 		off, err := file.ReadAt(buff, offset)
 		switch err {
 		case io.EOF:
@@ -51,23 +90,18 @@ func countWords(file *os.File, wordsChan chan<- string) error {
 			return err
 		}
 
-		off, err = processBatch(buff[:off], wordsChan)
-		if err != nil {
-			return err
-		}
+		processBatch(buff[:off], tk)
 
 		if done {
-			return nil
+			break
 		}
-
-		offset += int64(off)
 	}
+
+	return nil
 }
 
 // returns number of bytes processed
-func processBatch(batch []byte, wordsChan chan<- string) (int, error) {
-	processed := 0
-
+func processBatch(batch []byte, tk *topk.Stream) {
 	wordBuf := make([]byte, 16)
 	wordPos := 0
 
@@ -86,7 +120,7 @@ func processBatch(batch []byte, wordsChan chan<- string) (int, error) {
 				continue
 			}
 
-			wordsChan <- string(wordBuf[:wordPos])
+			tk.Insert(string(wordBuf[:wordPos]), 1)
 
 			wordPos = 0
 		case c < 'A' || c > 'z', c > 'Z' && c < 'a':
@@ -100,23 +134,14 @@ func processBatch(batch []byte, wordsChan chan<- string) (int, error) {
 			wordBuf[wordPos] = c
 			wordPos++
 		}
-
-		processed++
 	}
-
-	return processed, nil
 }
 
 const maxUint = ^uint(0)
 
-func processWords(wordsChan <-chan string, doneChan chan bool, topN int) {
-	tk := topk.New(topN)
+func processWords(wordsChan <-chan []byte, doneChan chan bool, tk *topk.Stream) {
 	for word := range wordsChan {
 		tk.Insert(string(word), 1)
-	}
-
-	for _, key := range tk.Keys() {
-		fmt.Printf("%d: %s\n", key.Count, key.Key)
 	}
 
 	close(doneChan)
--- main_test.go
+++ main_test.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"os"
 	"testing"
+
+	"github.com/dgryski/go-topk"
 )
+
+func Test(t *testing.T) {
+	tk := topk.New(10)
+	if err := fromFile("./assets/100000lines.txt", tk); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func Benchmark_read(b *testing.B) {
 	files := []string{
-		//"./assets/10lines.txt",
-		//"./assets/100lines.txt",
+		"./assets/test.txt",
+		"./assets/10lines.txt",
+		"./assets/100lines.txt",
 		"./assets/1000lines.txt",
+		"./assets/10000lines.txt",
+		"./assets/100000lines.txt",
 	}
 
 	for _, filePath := range files {
 		b.Run(filePath, func(b *testing.B) {
-			file, err := os.Open(filePath)
-			if err != nil {
-				b.Fatalf("failed to read `%s`: %s", filePath, err)
-			}
-
-			wordsChan := make(chan string, 100000)
-
-			go func() {
-				for range wordsChan {
-				}
-			}()
-
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				countWords(file, wordsChan)
+				tk := topk.New(10)
+				fromFile(filePath, tk)
 			}
 		})
 	}
